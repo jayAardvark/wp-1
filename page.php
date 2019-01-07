623b8adb4d716b3644ba3b9a91db9d6c29d8e967
Jr Cenina <44345078+jayAardvark@users.noreply.github.com>
2019-01-07 00:23:17 -0800

    minor changes

--- page.php
+++ page.php
@@ -1,7 +1,12 @@
 <?php
- while (have_posts()){
-    the_post(); ?>
-    <h2></h2>
-    <?php the_content();?> 
+
+   get_header();
+
+   while (have_posts()){
+      the_post(); ?>
+      <h2></h2>
+      <?php the_content();?> 
  <?php }
+
+   get_footer();
 ?> 
--- single.php
+++ single.php
@@ -1,7 +1,11 @@
 <?php
+
+   get_header();
  while (have_posts()){
     the_post(); ?>
     <h2></h2>
     <?php the_content();?> 
  <?php }
+
+   get_footer();
 ?> 
